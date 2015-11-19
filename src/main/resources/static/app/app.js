3d0abb96c7b280af0ab5a78363c38ed129cb3555
maciek <mwalerczuk@gmail.com>
2015-11-19 14:35:02 +0100

    web-socket works -> angularjs doesn't refresh binding

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -1,3 +1,11 @@
-var app = angular
-        .module('sicxe-sim', ['ngStomp', 'ngMessages', 'ui.router', 'sicxe-admin']);
+var sicxe = angular
+    .module('sicxe-sim', ['ngStomp', 'ngMessages', 'ui.router', 'sicxe-admin'])
+
+    //.run(function ($rootScope,$scope) {
+    //    $rootScope.$on('userLogged', function (event, args) {
+    //        $rootScope.$broadcast("handleUserLogged", args);
+    //
+    //    });
+    //
+    //});
 
--- src/main/resources/static/app/controllers.js
+++ src/main/resources/static/app/controllers.js
@@ -27,11 +27,11 @@ angular.module('sicxe-sim')
     .controller('TutorialsController', function () {
         console.log('tutorials');
     })
-    .controller('ProfileController', function ($scope, UserService) {
+    .controller('ProfileController', ['$scope','UserService', function ($scope, UserService) {
         $scope.user = UserService.getUser();
         console.log('profile');
-    })
-    .controller('LoginController', function ($scope, $state, UserService) {
+    }])
+    .controller('LoginController', ['$rootScope','$scope','$state','UserService', function ($rootScope, $scope, $state, UserService) {
         $scope.login = function(){
             $scope.user.logged = true;
             if($scope.user.username == "mwal"){
@@ -40,14 +40,17 @@ angular.module('sicxe-sim')
                 $scope.user.admin = false;
             }
             UserService.setUser($scope.user);
+            $rootScope.$broadcast('userLogged',"broadcast");
             $state.go('simulator', {}, {reload: true});
         }
-    })
-    .controller('HeaderController', function ($scope, $state, UserService) {
+    }])
+    .controller('HeaderController', ['$scope','UserService', function ($scope, UserService) {
         $scope.user = UserService.getUser();
-    })
-    .controller('OutputController', function($scope, $stomp, UserService){
+    }])
+    .controller('OutputController', ['$rootScope','$scope', '$stomp', 'UserService', function($rootScope, $scope, $stomp, UserService){
         $scope.messages = [];
+        $scope.mess = "";
+        $scope.test = true;
         $scope.connect = function () {
             $stomp
                 .connect('/update', {})
@@ -55,9 +58,10 @@ angular.module('sicxe-sim')
                         console.log("connected" + frame);
                         var subscription =
                             $stomp.subscribe('/topic/greetings', function (payload, headers, res) {
-                                $scope.$apply(function() {
-                                    $scope.messages.push(payload.content);
+                                $rootScope.$apply(function() {
+                                    $rootScope.mess = payload.content;
                                 });
+
                                 console.log(payload.content);
                             }, {});
                         var message = {name: "Maciek"};
@@ -74,13 +78,13 @@ angular.module('sicxe-sim')
                 });
 
         };
-        $scope.connect();
+        $rootScope.$on('userLogged', $scope.connect);
 
-    })
-    .controller('SignupController', function ($scope, $state) {
+    }])
+    .controller('SignupController', ['$scope','$state',function ($scope, $state) {
         $scope.signup = function(user) {
             $state.go('simulator', {}, {reload: true});
         }
-    });
+    }]);
 
 
